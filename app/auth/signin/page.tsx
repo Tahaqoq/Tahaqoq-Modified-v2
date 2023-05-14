d19f5743b7aed82d32b3904bf77d078aba242303
Nabster2010 <58792560+Nabster2010@users.noreply.github.com>
2023-05-14 13:28:54 +0300

    add access denied error to loginpage

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -39,6 +39,7 @@ export default function SignInPage() {
       router.push(callbackUrl || "/");
     });
     reset();
+    console.log(error);
   };
   return (
     <section className="min-h-screen bg-gray-50 dark:bg-gray-900">
@@ -112,6 +113,26 @@ export default function SignInPage() {
                       />
                     </svg>
                     <span>Error! Wrong credintials.</span>
+                  </div>
+                </div>
+              )}
+              {error && error === "AccessDenied" && (
+                <div className="shadow-lg alert alert-error">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="flex-shrink-0 w-6 h-6 stroke-current"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    <span>Error! AccessDenied.</span>
                   </div>
                 </div>
               )}
