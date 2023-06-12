5255ce25cb166a106b03fdd01d2f82fb48a1de24
Nabster2010 <58792560+Nabster2010@users.noreply.github.com>
2023-06-12 09:51:49 +0300

    Update site.ts activate Dammam

--- config/site.ts
+++ config/site.ts
@@ -1,32 +1,15 @@
 import { SiteConfig } from "@/types";
 
-// export const siteConfig: SiteConfig = {
-//   title: "TAHAQOQ ",
-//   branch: "DAMMAM",
-//   ar_branch: "الدمام",
-//   address: "Alkhalidiyah , Dammam ,KSA",
-//   ar_address: " الخالدية الجنوبية،الدمام،السعودية",
-//   phone: "0577929189",
-//   email: "info@tahaqoq.com",
-//   description: "VEHICLE INSPECTION CENTER",
-//   manager: "عبدالحكيم البريه",
-//   url: "https://tahaqoq.com/",
-//   ogImage: "",
-//   links: {
-//     twitter: "https://twitter.com/TahaqoqCo",
-//     github: "",
-//   },
-// };
 export const siteConfig: SiteConfig = {
   title: "TAHAQOQ ",
-  branch: "JEDDAH",
-  ar_branch: "جدة",
-  address: "Jawhara , Jeddah ,KSA",
-  ar_address: " الجوهرة،معارض السيارات،جدة،السعودية",
-  phone: "0502956801",
+  branch: "DAMMAM",
+  ar_branch: "الدمام",
+  address: "Alkhalidiyah , Dammam ,KSA",
+  ar_address: " الخالدية الجنوبية،الدمام،السعودية",
+  phone: "0577929189",
   email: "info@tahaqoq.com",
   description: "VEHICLE INSPECTION CENTER",
-  manager: "عبدالعزيز بانواس",
+  manager: "عبدالحكيم البريه",
   url: "https://tahaqoq.com/",
   ogImage: "",
   links: {
@@ -34,3 +17,20 @@ export const siteConfig: SiteConfig = {
     github: "",
   },
 };
+// export const siteConfig: SiteConfig = {
+//   title: "TAHAQOQ ",
+//   branch: "JEDDAH",
+//   ar_branch: "جدة",
+//   address: "Jawhara , Jeddah ,KSA",
+//   ar_address: " الجوهرة،معارض السيارات،جدة،السعودية",
+//   phone: "0502956801",
+//   email: "info@tahaqoq.com",
+//   description: "VEHICLE INSPECTION CENTER",
+//   manager: "عبدالعزيز بانواس",
+//   url: "https://tahaqoq.com/",
+//   ogImage: "",
+//   links: {
+//     twitter: "https://twitter.com/TahaqoqCo",
+//     github: "",
+//   },
+// };
